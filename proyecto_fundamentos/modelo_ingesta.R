8850edbf7c8de09c7360ff34d6f9debd74e774cb
marianaga89 <marianagodina@gmail.com>
2017-12-19 00:20:26 -0600

    modelo ingesta

--- proyecto_fundamentos/modelo_ingesta.R
+++ proyecto_fundamentos/modelo_ingesta.R
@@ -26,7 +26,12 @@ base_ingesta <- bd_diabetes %>%
   mutate(ingesta = ifelse(ingesta == 0, 2, ingesta),
          unos = 1) %>% 
   group_by(individual, ingesta) %>% 
-  summarise(media_glu = mean(medicion))
+  summarise(media_glu = mean(medicion)) %>% 
+  filter(media_glu>2)
+
+base_ingesta %>% 
+  ggplot(aes(x = 1:104, y = media_glu, color=as.factor(ingesta))) +
+  geom_point()
 
 n <- nrow(base_ingesta)
 
@@ -36,8 +41,8 @@ inits<-function(){list(alpha = rep(0,n), beta = rep(0,2), yf1=rep(1,n))}
 
 parameters<-c("alpha.adj","beta.adj", "yf1")
 
-mod.reg <-jags(data,inits,parameters,model.file="modelo_prueba.txt",
-               n.iter=10000,n.chains=1,n.burnin=1000, n.thin = 1)
+mod.reg <-jags(data,inits,parameters,model.file="modelo_ingesta.txt",
+               n.iter=30000,n.chains=1,n.burnin=3000, n.thin = 1)
 
 out.dic<-mod.reg$BUGSoutput$DIC
 print(out.dic)
@@ -54,10 +59,17 @@ data.frame(y_obs = base_ingesta$media_glu, y_pred = out.yf[,1]) %>%
   geom_point() +
   geom_smooth(method = "lm")
 
-data.frame(y_obs = base_ingesta$media_glu, y_pred = out.yf[,1]) %>% 
+data.frame(y_obs = base_ingesta$media_glu, y_pred = out.yf[,1], y_inf = out.yf[,3],
+           y_sup = out.yf[,7]) %>% 
   ggplot() +
   geom_point(aes(x = 1:n, y = y_obs)) +
-  geom_point(aes(x = 1:n, y = y_pred), color = "red")
+  geom_point(aes(x = 1:n, y = y_pred), color = "red") +
+  geom_errorbar(aes(x = 1:n, ymin = y_inf, ymax=y_sup))  +
+  labs(x = "Observaciones", y = "Glucosa")
 
-
+data.frame(Beta1 = out$beta.adj[,1], Beta2 = out$beta.adj[,2]) %>% 
+  gather() %>% 
+  ggplot(aes(x = value, fill = key)) +
+  geom_histogram(alpha = .3) +
+  scale_fill_discrete(name = "Parámetro")
 
